71a61dce3c5e8e69e8d70d1181eef291d89f933e
TheBleak13 <34757963+thebleak13@users.noreply.github.com>
2023-06-10 15:43:47 -0700

    Fixed #14

--- SQL/CHData - Number of time a weapon type is used in a mass shooting.sql
+++ SQL/CHData - Number of time a weapon type is used in a mass shooting.sql
@@ -3,6 +3,6 @@ sum(case when weapon_type like '%Semiautomatic handgun%' then 1 else 0 end) as s
 sum(case when weapon_type like '%semiautomatic rifle%' then 1 else 0 end) as semiautomatic_rifle,
 sum(case when weapon_type like '%shotgun%' then 1 else 0 end) as shotgun,
 sum(case when weapon_type like '%revolver%' then 1 else 0 end) as revolver,
-sum(case when weapon_type like '%revolver%' then 1 else 0 end) as derringer,
+sum(case when weapon_type like '%derringer%' then 1 else 0 end) as derringer,
 sum(case when weapon_type like '%knife%' then 1 else 0 end) as knife
 from CHData
