dd008e3fdd2a46dfecc72e492821a599bcc17e6b
TheBleak13 <34757963+thebleak13@users.noreply.github.com>
2023-06-10 21:10:24 -0700

    Update CHData - Shooting Locations (City).sql
    Updated query to include the percentage

--- SQL/CHData - Shooting Locations (City).sql
+++ SQL/CHData - Shooting Locations (City).sql
@@ -1,8 +1,9 @@
-SELECT location
-, count(1) as Shootings
-, sum(TOTAL_VICTIMS) as Victims
-, sum(TOTAL_VICTIMS)/count(1) as VictimsPerShooting 
-
+SELECT 
+  location, 
+  count(1) as Shootings, 
+  sum(TOTAL_VICTIMS) as Victims, 
+  sum(TOTAL_VICTIMS)/count(1) as VictimsPerShooting,
+  ROUND(count(1) * 100.0 / (SELECT COUNT(*) FROM CHData), 2) as ShootingPercentage
 FROM CHData
 GROUP BY location
-ORDER BY Shootings DESC
+ORDER BY Shootings DESC;
