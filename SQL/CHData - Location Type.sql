abf8992f5af0ff36dc9cdeed59151088ae02297c
TheBleak13 <34757963+thebleak13@users.noreply.github.com>
2023-06-10 17:00:34 -0700

    Fixed #13

--- SQL/CHData - Location Type.sql
+++ SQL/CHData - Location Type.sql
@@ -1,7 +1,7 @@
 SELECT 
   location_2, 
   count(1) as Count,
-  ROUND(count(1) * 100.0 / SUM(count(1)) OVER (), 2) as Percentage
+  ROUND(count(1) * 100.0 / (SELECT COUNT(*) FROM CHData), 2) as Percentage
 FROM CHData
 GROUP BY location_2
 ORDER BY COUNT DESC;
