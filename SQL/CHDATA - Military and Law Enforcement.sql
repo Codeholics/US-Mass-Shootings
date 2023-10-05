2e51bdf3d9b9bf2eb7f4f7e5574856c7959b4ab4
TheBleak <thebleak13@gmail.com>
2023-10-05 09:23:11 -0700

    Create CHDATA - Military and Law Enforcement.sql

---
+++ SQL/CHDATA - Military and Law Enforcement.sql
@@ -0,0 +1,11 @@
+select * from CHData 
+where summary like '%military%' 
+or summary like '%retired sergeant%' 
+or summary like '%ex-US Air Force%' 
+or summary like '%former Marine%' 
+or summary like '%Army veteran%' 
+or summary like '%Army Specialist' 
+or summary like '%veteran of the Virginia Army%'
+or summary like '%Off-duty sheriff''s deputy%'
+or summary like '%Army psychiatrist%'
+or summary like '%three-year stint in the US Army%'
