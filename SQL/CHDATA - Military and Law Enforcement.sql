a4628ba6fdafb38b7c3706220708805bb81adbd5
TheBleak <thebleak13@gmail.com>
2023-10-10 12:42:39 -0700

    Update CHDATA - Military and Law Enforcement.sql

--- SQL/CHDATA - Military and Law Enforcement.sql
+++ SQL/CHDATA - Military and Law Enforcement.sql
@@ -1,11 +1,22 @@
-select * from CHData 
-where summary like '%military%' 
-or summary like '%retired sergeant%' 
-or summary like '%ex-US Air Force%' 
-or summary like '%former Marine%' 
-or summary like '%Army veteran%' 
-or summary like '%Army Specialist' 
-or summary like '%veteran of the Virginia Army%'
-or summary like '%Off-duty sheriff''s deputy%'
-or summary like '%Army psychiatrist%'
-or summary like '%three-year stint in the US Army%'
+DROP TABLE IF EXISTS temp_table;
+CREATE TEMPORARY TABLE temp_table AS
+SELECT * FROM CHData 
+WHERE summary LIKE '%military%' 
+OR summary LIKE '%retired sergeant%' 
+OR summary LIKE '%ex-US Air Force%' 
+OR summary LIKE '%former Marine%' 
+OR summary LIKE '%Army veteran%' 
+OR summary LIKE '%Army Specialist' 
+OR summary LIKE '%veteran of the Virginia Army%'
+OR summary LIKE '%Off-duty sheriff''s deputy%'
+OR summary LIKE '%Army psychiatrist%'
+OR summary LIKE '%three-year stint in the US Army%';
+
+SELECT CH.*, 
+  CASE WHEN T.summary IS NOT NULL THEN 'Y' ELSE 'N' END AS trained
+FROM CHData AS CH
+LEFT JOIN temp_table AS T
+ON CH.summary = T.summary;
+
+--DROP TABLE temp_table;
+--SELECT * FROM temp_table;
