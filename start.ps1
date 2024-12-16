06e15af7254864ddf6830f6be119eb026fe9b655
Eric Reis <34757963+EReis0@users.noreply.github.com>
2024-12-16 09:34:39 -0800

    Fixed #30

--- start.ps1
+++ start.ps1
@@ -66,93 +66,122 @@ try {
 Write-LogInfo -LogPath $LogFilePath -Message "[$(Get-Date)] Starting column data correction loop" -ToScreen
 $Data = @()
 foreach($item in $Spreadsheet) {
+
+    # Variable Assignments
+    $gender = $item.gender.length
+    $type = $item.type
+    $mental_health_sources = $item.mental_health_sources
+    $age_of_shooter = $item.age_of_shooter
+    $weapon_details = $item.weapon_details
+    $latitude = $item.latitude
+    $longitude = $item.longitude
+    $year = $item.year
+    $changes = $item.changes
+    $sources = $item.sources
+    $sources_additional_age = $item.sources_additional_age
+    $total_victims = $item.total_victims
+    $injured = $item.injured
+    $fatalities = $item.fatalities
+    $summary = $item.summary
+    $date = $item.date
+    $location = $item.location
+    $case = $item.case
+    $mental_health_details = $item.mental_health_details
+    $where_obtained = $item.where_obtained
+    $weapons_obtained_legally = $item.weapons_obtained_legally
+    $weapon_type = $item.weapon_type
+    $race = $item.race
+    $location_2 = $item.location_2
+    $prior_signs_mental_health_issues = $item.prior_signs_mental_health_issues
+    
     
     #Splitting city out of location
-    $CityRaw = $item.location
+    $CityRaw = $location
     $CityOnly = $CityRaw -replace ',.*',''
     $City = $CityOnly.trim('')
     
     #Splitting out state from location
-    $StateRaw = $item.location
+    $StateRaw = $location
     $StateOnly = $StateRaw -replace '.*,',''
-    $State  = $StateOnly.trim('')
+    $state  = $StateOnly.trim('')
 
     #force first character of race caps
-    $RaceCaps = $item.race
-    $Race = $RaceCaps.toCharArray()[0].tostring().toUpper() + $RaceCaps.remove(0,1)
-    $Race = $Race -replace '^[-]+$', ''
+    $RaceCaps = $race
+    $race = $RaceCaps.toCharArray()[0].tostring().toUpper() + $RaceCaps.remove(0,1)
+    $race = $race -replace '^[-]+$', ''
 
     #force first character of location_2 caps
-    $LocationCaps = $item.location_2
-    $Location_2 = $LocationCaps.toCharArray()[0].tostring().toUpper() + $LocationCaps.remove(0, 1)
-    $Location_2 = $Location_2.trim('')
+    $LocationCaps = $location_2
+    $location_2 = $LocationCaps.toCharArray()[0].tostring().toUpper() + $LocationCaps.remove(0, 1)
+    $location_2 = $location_2.trim('')
     
     #force first character of prior_signs_mental_health_issues caps
-    $PriorSignsMentalHealthIssuesCaps = $item.prior_signs_mental_health_issues
+    $PriorSignsMentalHealthIssuesCaps = $prior_signs_mental_health_issues
     $PriorSignsMentalHealthIssues = $PriorSignsMentalHealthIssuesCaps.toCharArray()[0].tostring().toUpper() + $PriorSignsMentalHealthIssuesCaps.remove(0, 1)
     $PriorSignsMentalHealthIssues = $PriorSignsMentalHealthIssues -replace '^[-]+$', ''
 
     #force first character of weapon_type
-    $WeaponTypeCaps = $item.weapon_type
+    $WeaponTypeCaps = $weapon_type
     $WeaponType = $WeaponTypeCaps.toCharArray()[0].tostring().toUpper() + $WeaponTypeCaps.remove(0, 1)
 
     #force first character of gender caps 
-    $GenderCaps = $item.gender
-    $Gender = $GenderCaps.toCharArray()[0].tostring().toUpper() + $GenderCaps.remove(0,1)
+    $GenderCaps = $gender
+    $gender = $GenderCaps.toCharArray()[0].tostring().toUpper() + $GenderCaps.remove(0,1)
     
     #force first character of weapons_obtained_legally caps
-    $weapons_obtained_legallyCaps = $item.weapons_obtained_legally
+    $weapons_obtained_legallyCaps = $weapons_obtained_legally
     $weapons_obtained_legally = $weapons_obtained_legallyCaps.toCharArray()[0].tostring().toUpper() + $weapons_obtained_legallyCaps.remove(0,1)
     $weapons_obtained_legally = $weapons_obtained_legally -replace '^[-]+$', ''
 
     #force first character of gender caps
-    $where_obtainedCaps = $item.where_obtained
+    $where_obtainedCaps = $where_obtained
     $where_obtained = $where_obtainedCaps.toCharArray()[0].tostring().toUpper() + $where_obtainedCaps.remove(0,1)
     $where_obtained = $where_obtained -replace '^[-]+$', ''
 
     #force first character of mental_health_details caps
-    $mental_health_detailsCaps = $item.mental_health_details
+    $mental_health_detailsCaps = $mental_health_details
     $mental_health_details = $mental_health_detailsCaps.toCharArray()[0].tostring().toUpper() + $mental_health_detailsCaps.remove(0,1)
     $mental_health_details = $mental_health_details -replace '^[-]+$', ''
 
     # Removing - from age_of_shooter
-    $age_of_shooter = $item.age_of_shooter -replace '^[-]+$', ''
+    $age_of_shooter = $age_of_shooter -replace '^[-]+$', ''
 
     # Removing - from weapon_details
-    $weapon_details = $item.weapon_details -replace '^[-]+$', ''
+    $weapon_details = $weapon_details -replace '^[-]+$', ''
 
     # Removing - from mental_health_sources
-    $mental_health_sources = $item.mental_health_sources -replace '^[-]+$', ''
+    $mental_health_sources = $mental_health_sources -replace '^[-]+$', ''
 
     # Removing - from latitude
-    $latitude = $item.latitude -replace '^[-]+$', ''
+    $latitude = $latitude -replace '^[-]+$', ''
 
     # Removing - from longitude
-    $longitude = $item.longitude -replace '^[-]+$', ''
+    $longitude = $longitude -replace '^[-]+$', ''
 
     #the gender values are not consistnt, abbreviating the gender value for more consistent data
-    $GenderValueLength = $item.gender.length
+    $GenderValueLength = $gender
     if($GenderValueLength -eq 4){
-        $Gender = 'M'
+        $gender = 'M'
     }elseif ($GenderValueLength -eq 6) {
-        $Gender = 'F'
+        $gender = 'F'
     }elseif($GenderValueLength -eq 13){
-        $Gender = 'M/F'
+        $gender = 'M/F'
     }else{
         #Do nothing if the value length is 1. (result already "M", "F" or "M/F")
+        $gender
     }
 
     # Final Data Array for CH Edition
     $Data += [PSCustomObject]@{
-        case = $item
-        location = $item.location
+        case = $case
+        location = $location
         city = $City
         state = $State
-        date = $item.date
-        summary = $item.summary
-        fatalities = $item.fatalities
-        injured = $item.injured
-        total_victims = $item.total_victims
+        date = $date
+        summary = $summary
+        fatalities = $fatalities
+        injured = $injured
+        total_victims = $total_victims
         location_2 = $Location_2
         age_of_Shooter = $age_of_shooter
         prior_signs_mental_health_issues = $PriorSignsMentalHealthIssues
@@ -161,18 +190,17 @@ foreach($item in $Spreadsheet) {
         where_obtained = $where_obtained
         weapon_type = $WeaponType
         weapon_details = $weapon_details
-        race = $Race
-        gender = $Gender
-        sources = $item.sources
+        race = $race
+        gender = $gender
+        sources = $sources
         mental_health_sources = $mental_health_sources
-        sources_additional_age = $item.sources_additional_age
+        sources_additional_age = $sources_additional_age
         latitude = $latitude
         longitude = $longitude
-        type = $item.type
-        year = $item.year
-        changes = $item.changes
+        type = $type
+        year = $year
+        changes = $changes
     }
-
 } 
 
 
