252682bbfcb320940110550e323ad3f1008e99a8
Eric Reis <34757963+EReis0@users.noreply.github.com>
2024-12-19 11:40:30 -0800

    Update SQLPort.ps1

--- dev/SQLPort.ps1
+++ dev/SQLPort.ps1
@@ -2,23 +2,78 @@ Import-Module -Name PSSQLite, ImportExcel, PSLogging
 
 $CPSScriptRoot = "D:\Code\Repos\US-Mass-Shootings\dev"
 
-# Function to escape single quotes in SQL string
-. "$CPSScriptRoot\functions\Get-EscapeSQLString.ps1"
+# Importing functions
+$GetMotherJonesDB = Join-Path -Path $CPSScriptRoot -ChildPath 'Functions' | Join-Path -ChildPath 'Get-MotherJonesDB.ps1'
+$NewSQLiteDB = Join-Path -Path $CPSScriptRoot -ChildPath 'Functions' | Join-Path -ChildPath 'New-SQLiteDB.ps1'
+$EscapeSQLString = Join-Path -Path $CPSScriptRoot -ChildPath 'Functions' | Join-Path -ChildPath 'Get-EscapeSQLString.ps1'
+. $GetMotherJonesDB
+. $NewSQLiteDB
+    # Function to escape single quotes in SQL string
+. $EscapeSQLString
 
+# Variables
+$Date = Get-Date -Format "yyyyMMdd"
+$Random = Get-Random
+$ExportPath = Join-Path -Path $CPSScriptRoot -ChildPath 'Export'
 
-##########################
-## SQLite Section Below
-##########################
+# SQLite Variables
+$SQLitePath = Join-Path -Path $CPSScriptRoot -ChildPath 'Resources' | Join-Path -ChildPath 'System.Data.SQLite.dll'
+$DBPath = Join-Path -Path $ExportPath -ChildPath 'MassShooterDatabase.sqlite'
+
+# Import and Export FileName Variables
+#ExportWebView = Join-Path -Path $ExportPath -ChildPath 'WebView.html'
+$ExportCHEdition = Join-Path -Path $ExportPath -ChildPAth 'Codeholics - Mass Shootings Database 1982-2024.csv'
+$ImportCSVPath = Join-Path -Path $ExportPath -ChildPath 'Mother Jones - Mass Shootings Database 1982-2024.csv'
+
+# Log Variables
+$LogPath = Join-Path -Path $CPSScriptRoot -ChildPath 'Logs'
+$LogName = "$Date-$Random.log"
+$LogFilePath = Join-Path -Path $LogPath -ChildPath $LogName
+$Version = "2.0"
+
+# Start Logging
+Start-Log -LogPath $LogPath -LogName $LogName -ScriptVersion $Version
+
+########################
+## Test SQL File
+########################
 
 # This is just to test what the sql output would look like for formatting issues
-$TestSQLFile = "D:\Code\Repos\US-Mass-Shootings\dev\test.sql"
-if (Test-Path -Path $TestSQLFile) {
-    Remove-Item -Path $TestSQLFile -Force
-    Write-Host "File removed: $TestSQLFile"
+$CHTestSQLFile = "$CPSScriptRoot\Export\CHtest.sql"
+if (Test-Path -Path $CHTestSQLFile) {
+    Remove-Item -Path $CHTestSQLFile -Force
+    Write-Host "File removed: $CHTestSQLFile"
 } else {
-    Write-Host "File does not exist: $TestSQLFile"
+    Write-Host "File does not exist: $CHTestSQLFile"
 }
 
+$MJTestSQLFile = "$CPSScriptRoot\Export\MJtest.sql"
+if (Test-Path -Path $MJTestSQLFile) {
+    Remove-Item -Path $MJTestSQLFile -Force
+    Write-Host "File removed: $MJTestSQLFile"
+} else {
+    Write-Host "File does not exist: $MJTestSQLFile"
+}
+
+########################
+## SQLite Create/Connect
+########################
+
+# Create SQLite DB
+try {
+    New-SQLiteDB -DirRoot $CPSScriptRoot -SQLitePath $SQLitePath -DBPath $DBPath
+    Write-LogInfo -LogPath $LogFilePath -Message "[$(Get-Date)] SQLite DB Created [$SQLitePath]" -ToScreen
+}catch{
+    Write-LogError -LogPath $LogFilePath -Message "[$(Get-Date)] Creating SQLite DB [$SQLitePath]" -ToScreen
+}
+
+# Connect to the SQLite DB
+try {
+    $Connection = New-SqliteConnection -DataSource $DBPath
+    Write-LogInfo -LogPath $LogFilePath -Message "[$(Get-Date)] Connected to SQLite DB [$DBPath]" -ToScreen
+}catch{
+    Write-LogError -LogPath $LogFilePath -Message "[$(Get-Date)] Connecting to SQLite DB [$DBPath]" -ToScreen
+}
 
 ########################
 ## CH Edition SQLite
@@ -27,14 +82,72 @@ if (Test-Path -Path $TestSQLFile) {
 # Import the CH Edition and insert records into SQLite DB
 $CH_TableName = 'CHData'
 try{
-    $CH_CSV = $DataFinal
-    #$CH_CSV = Import-CSV -Path $ExportCHEdition | Sort-Object -Property {[DateTime]::ParseExact($_.date,'yyyy-MM-dd',$null)}
-    #Write-LogInfo -LogPath $LogFilePath -Message "[$(Get-Date)] Importing CH Edition [$ExportCHEdition]" -ToScreen
+    #$CH_CSV = $DataFinal
+    $CH_CSV = Import-CSV -Path $ExportCHEdition | Sort-Object -Property {[DateTime]::ParseExact($_.date,'yyyy-MM-dd',$null)}
+    Write-LogInfo -LogPath $LogFilePath -Message "[$(Get-Date)] Importing CH Edition [$ExportCHEdition]" -ToScreen
 }catch{
-    #Write-LogError -LogPath $LogFilePath -Message "[$(Get-Date)] Importing CH Edition [$ExportCHEdition]" -ToScreen
+    Write-LogError -LogPath $LogFilePath -Message "[$(Get-Date)] Importing CH Edition [$ExportCHEdition]" -ToScreen
 }
 
 foreach ($item in $CH_CSV) {
+    $case = Get-EscapeSQLString $item.case
+    $location = Get-EscapeSQLString $itemlocation
+    $city = Get-EscapeSQLString $item.city
+    $state = Get-EscapeSQLString $item.state
+    $date = Get-EscapeSQLString $item.date
+    $summary = Get-EscapeSQLString $item.summary
+    $fatalities = Get-EscapeSQLString $item.fatalities
+    $injured = Get-EscapeSQLString $item.injured
+    $total_victims = Get-EscapeSQLString $item.total_victims
+    $location_2 = Get-EscapeSQLString $item.location_2
+    $age_of_Shooter = Get-EscapeSQLString $item.age_of_Shooter
+    $prior_signs_mental_health_issues = Get-EscapeSQLString $item.prior_signs_mental_health_issues
+    $mental_health_details = Get-EscapeSQLString $item.mental_health_details
+    $weapons_obtained_legally = Get-EscapeSQLString $item.weapons_obtained_legally
+    $where_obtained = Get-EscapeSQLString $item.where_obtained
+    $weapon_type = Get-EscapeSQLString $item.weapon_type
+    $weapon_details = Get-EscapeSQLString $item.weapon_details
+    $race = Get-EscapeSQLString $item.race
+    $gender = Get-EscapeSQLString $item.gender
+    $sources = Get-EscapeSQLString $item.sources
+    $mental_health_sources = Get-EscapeSQLString $item.mental_health_sources
+    $sources_additional_age = Get-EscapeSQLString $item.sources_additional_age
+    $latitude = Get-EscapeSQLString $item.latitude
+    $longitude = Get-EscapeSQLString $item.longitude
+    $type = Get-EscapeSQLString $item.type
+    $year = Get-EscapeSQLString $item.year
+    $changes = Get-EscapeSQLString $item.changes
+
+    # SQL Query to insert records into SQLite DB
+    $CH_Query = "INSERT INTO $CH_TableName ([case], location, city, state, date, summary, fatalities, injured, total_victims, location_2, age_of_Shooter, prior_signs_mental_health_issues, mental_health_details, weapons_obtained_legally, where_obtained, weapon_type, weapon_details, race, gender, sources, mental_health_sources, sources_additional_age, latitude, longitude, type, year, changes) VALUES 
+    ('[$case]','$location','$city','$state','$date','$summary','$fatalities','$injured','$total_victims','$location_2','$age_of_Shooter','$prior_signs_mental_health_issues','$mental_health_details','$weapons_obtained_legally','$where_obtained','$weapon_type','$weapon_details','$race','$gender','$sources','$mental_health_sources','$sources_additional_age','$latitude','$longitude','$type','$year','$changes')"
+    
+    $CH_Query | Out-File -FilePath $CHTestSQLFile -Append
+
+    try {
+        #Invoke-SqliteQuery -Connection $Connection -Query $CH_Query
+        Write-LogInfo -LogPath $LogFilePath -Message "[$(Get-Date)] Inserted into CH Edition DB: [$_case : $date]" -ToScreen
+    } catch {
+        Write-LogError -LogPath $LogFilePath -Message "[$(Get-Date)] Inserting into CH Edition DB: [$CH_Query]" -ToScreen
+    }
+
+}
+
+########################
+## MJ Edition SQLite
+########################
+
+# Import the MJ Edition and insert records into SQLite DB
+$MJ_TableName = 'MJData'
+try{
+    #$MJ_CSV = $DataFinal
+    $MJ_CSV = Import-CSV -Path $ImportCSVPath | Sort-Object -Property {[DateTime]::ParseExact($_.date,'yyyy-MM-dd',$null)}
+    Write-LogInfo -LogPath $LogFilePath -Message "[$(Get-Date)] Imported MJ Edition [$ImportCSVPath]" -ToScreen
+}catch{
+    Write-LogError -LogPath $LogFilePath -Message "[$(Get-Date)] Importing MJ Edition [$ImportCSVPath]" -ToScreen
+}
+
+foreach ($item in $MJ_CSV) {
     $case = Get-EscapeSQLString $item.case
     $location = Get-EscapeSQLString $itemlocation
     $date = Get-EscapeSQLString $item.date
@@ -61,18 +174,19 @@ foreach ($item in $CH_CSV) {
     $year = Get-EscapeSQLString $item.year
 
     # SQL Query to insert records into SQLite DB
-    $CH_Query = "INSERT INTO $CH_TableName ([case], location, date, summary, fatalities, injured, total_victims, location_2, age_of_Shooter, prior_signs_mental_health_issues, mental_health_details, weapons_obtained_legally, where_obtained, weapon_type, weapon_details, race, gender, sources, mental_health_sources, sources_additional_age, latitude, longitude, type, year) VALUES 
+    $MJ_Query = "INSERT INTO $MJ_TableName ([case], location, date, summary, fatalities, injured, total_victims, location_2, age_of_Shooter, prior_signs_mental_health_issues, mental_health_details, weapons_obtained_legally, where_obtained, weapon_type, weapon_details, race, gender, sources, mental_health_sources, sources_additional_age, latitude, longitude, type, year) VALUES 
     ('[$case]','$location','$date','$summary','$fatalities','$injured','$total_victims','$location_2','$age_of_Shooter','$prior_signs_mental_health_issues','$mental_health_details','$weapons_obtained_legally','$where_obtained','$weapon_type','$weapon_details','$race','$gender','$sources','$mental_health_sources','$sources_additional_age','$latitude','$longitude','$type','$year')"
     
-    $CH_Query | Out-File -FilePath $TestSQLFile -Append
-    #Invoke-SqliteQuery -Connection $Connection -Query $MJ_Query
+    $MJ_Query | Out-File -FilePath $MJTestSQLFile -Append
+
+    try {
+        #Invoke-SqliteQuery -Connection $Connection -Query $MJ_Query
+        Write-LogInfo -LogPath $LogFilePath -Message "[$(Get-Date)] Inserted into MJ Edition DB: [$_case : $date]" -ToScreen
+    } catch {
+        Write-LogError -LogPath $LogFilePath -Message "[$(Get-Date)] Inserting into MJ Edition DB: [$MJ_Query]" -ToScreen
+    }
 
 }
 
-########################
-## MJ Edition SQLite
-########################
-
-<#
-Insert SQL insert like above but for MJ Edition
-#>
+# Close the SQLite Connection
+$Connection.Close()
