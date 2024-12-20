789612536ee4d606668d8c39775716a5dcfebb44
Eric Reis <34757963+EReis0@users.noreply.github.com>
2024-12-19 16:30:31 -0800

    Update Get-MotherJonesDB.ps1

--- dev/Functions/Get-MotherJonesDB.ps1
+++ dev/Functions/Get-MotherJonesDB.ps1
@@ -1,5 +1,4 @@
 
-function Get-MotherJonesDB {
 <#
     .SYNOPSIS
         Downloads and processes data from the Mother Jones Mass Shooting Database.
@@ -12,35 +11,66 @@ function Get-MotherJonesDB {
     .PARAMETER ExportFixed
         Specifies the file path for the processed CSV file.
     .EXAMPLE
-        $CPSScriptRoot = "D:\Github\PowerShell\R AND D\Data World\MJ Mass Shooter Database"
+        $CPSScriptRoot = "D:\code\MJ Mass Shooter Database"
         Get-MotherJonesDB -Output "$CPSScriptRoot\Export"
 #>
+function Get-MotherJonesDB {
     [cmdletbinding()]
     param (
         [Parameter(Mandatory=$true)]
         [string]$Output,
 
         [Parameter(Mandatory=$false)]
-        [string]$ExportOG = "$Output\Mother Jones Raw.csv",
+        [string]$ExportOG = (Join-Path $Output -ChildPath "Mother Jones Raw.csv"),
 
         [Parameter(Mandatory=$false)]
-        [string]$ExportFixed = "$Output\Mother Jones - Mass Shootings Database 1982-2024.csv"
+        [string]$ExportFixed = (Join-Path -Path $Output -ChildPath "Mother Jones - Mass Shootings Database 1982-2024.csv")
     )
-    #Mother Jones Mass Shooting Database
-    #Mother Jones - Mass Shootings Database, 1982 - 2022
+
+    # Mother Jones Mass Shooting Database
+    # Mother Jones - Mass Shootings Database, 1982 - 2024
     $Mothers = 'https://docs.google.com/spreadsheets/d/1b9o6uDO18sLxBqPwl_Gh9bnhW-ev_dABH83M5Vb5L8o/export?format=csv'
 
+    # Remove existing files
     if (test-path $ExportOG) {Remove-Item $ExportOG}
     if (test-path $ExportFixed) {Remove-Item $ExportFixed}
 
+    # Download the Mother Jones DB CSV
     Invoke-WebRequest -outFile $ExportOG $Mothers
 
+    # Import CSV and skip the first row (header) - Header is manually defined (fix for duplicate "location" column)
     $csv = Import-CSV -Path $ExportOG -Header "case","location","date","summary","fatalities","injured","total_victims","location_2","age_of_shooter","prior_signs_mental_health_issues","mental_health_details","weapons_obtained_legally","where_obtained","weapon_type","weapon_details","race","gender","sources","mental_health_sources","sources_additional_age","latitude","longitude","type","year" | Select-Object -Skip 1
 
-    #testing code here
     # Fix Date format to always be M/d/yyyy. Also escape single quotes.
     $DataFix = @()
     foreach ($item in $csv) {
+
+        # Initialize variables
+        $case = $null
+        $location = $null
+        $date = $null
+        $summary = $null
+        $fatalities = $null
+        $injured = $null
+        $total_victims = $null
+        $location_2 = $null
+        $age_of_Shooter = $null
+        $prior_signs_mental_health_issues = $null
+        $mental_health_details = $null
+        $weapons_obtained_legally = $null
+        $where_obtained = $null
+        $weapon_type = $null
+        $weapon_details = $null
+        $race = $null
+        $gender = $null
+        $sources = $null
+        $mental_health_sources = $null
+        $latitude = $null
+        $longitude = $null
+        $type = $null
+        $year = $null
+
+        # Variable assignments
         $Summary = $item.summary -replace "'", "''"
         $MentalHealthDetails = $item.mental_health_details -replace "'", "''"
         $Case = $item.case -replace "'", "''"
@@ -48,41 +78,38 @@ function Get-MotherJonesDB {
         $WeaponDetails = $item.weapon_details -replace "'", "''"
         $WeaponType = $item.weapon_type -replace "'", "''"
         $Date = (Get-Date $item.date).ToString("yyyy-MM-dd")
-        
-        $row = new-object psobject
-        $row | add-member -MemberType NoteProperty -Name case -Value $Case
-        $row | add-member -MemberType NoteProperty -Name location -Value $item.location
-        #$row | add-member -MemberType NoteProperty -Name city -Value $item.city
-        #$row | add-member -MemberType NoteProperty -Name state -Value $item.state
-        $row | add-member -MemberType NoteProperty -Name date -Value $Date
-        $row | add-member -MemberType NoteProperty -Name summary -Value $Summary
-        $row | add-member -MemberType NoteProperty -Name fatalities -Value $item.fatalities
-        $row | add-member -MemberType NoteProperty -Name injured -Value $item.injured
-        $row | add-member -MemberType NoteProperty -Name total_victims -Value $item.total_victims
-        $row | add-member -MemberType NoteProperty -Name location_2 -Value $item.location_2
-        $row | add-member -MemberType NoteProperty -Name age_of_Shooter -Value $item.age_of_Shooter
-        $row | add-member -MemberType NoteProperty -Name prior_signs_mental_health_issues -Value $item.prior_signs_mental_health_issues
-        $row | add-member -MemberType NoteProperty -Name mental_health_details -Value $MentalHealthDetails
-        $row | add-member -MemberType NoteProperty -Name weapons_obtained_legally -Value $item.weapons_obtained_legally
-        $row | add-member -MemberType NoteProperty -Name where_obtained -Value $WhereObtained
-        $row | add-member -MemberType NoteProperty -Name weapon_type -Value $WeaponType
-        $row | add-member -MemberType NoteProperty -Name weapon_details -Value $WeaponDetails
-        $row | add-member -MemberType NoteProperty -Name race -Value $item.race
-        $row | Add-member -MemberType NoteProperty -Name gender -Value $item.gender
-        $row | add-member -MemberType NoteProperty -Name sources -Value $item.sources
-        $row | add-member -MemberType NoteProperty -Name mental_health_sources -Value $item.mental_health_sources
-        $row | add-member -MemberType NoteProperty -Name latitude -Value $item.latitude
-        $row | add-member -MemberType NoteProperty -Name longitude -Value $item.longitude
-        $row | add-member -MemberType NoteProperty -Name type -Value $item.type
-        $row | add-member -MemberType NoteProperty -Name year -Value $item.year
-        $DataFix += $row
 
+        # Add to $DataFix array
+        $DataFix += [PSCustomObject]@{
+            case = $Case
+            location = $item.location
+            date = $Date
+            summary = $Summary
+            fatalities = $item.fatalities
+            injured = $item.injured
+            total_victims = $item.total_victims
+            location_2 = $item.location_2
+            age_of_Shooter = $item.age_of_Shooter
+            prior_signs_mental_health_issues = $item.prior_signs_mental_health_issues
+            mental_health_details = $MentalHealthDetails
+            weapons_obtained_legally = $item.weapons_obtained_legally
+            where_obtained = $WhereObtained
+            weapon_type = $WeaponType
+            weapon_details = $WeaponDetails
+            race = $item.race
+            gender = $item.gender
+            sources = $item.sources
+            mental_health_sources = $item.mental_health_sources
+            latitude = $item.latitude
+            longitude = $item.longitude
+            type = $item.type
+            year = $item.year
+        }
     }
-
 
     #Export-CSV -Path $ExportFixed -NoTypeInformation
     $DataFix | Export-CSV -Path $ExportFixed -NoTypeInformation
 }
 
-#$CPSScriptRoot = "D:\Github\PowerShell\R AND D\Data World\MJ Mass Shooter Database"
+#$CPSScriptRoot = "D:\Code\MJ Mass Shooter Database"
 #Get-MotherJonesDB -Output "$CPSScriptRoot\Export"
